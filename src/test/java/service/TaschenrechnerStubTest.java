41007955039b08f07f0d62ff1ff49621614d6c66
Thomas Much <github@snailshell.de>
2023-09-10 13:19:17 +0200

    Add stub test

---
+++ src/test/java/service/TaschenrechnerStubTest.java
@@ -0,0 +1,35 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TaschenrechnerStubTest {
+
+    @Mock
+    RechenengineMitDatenbankanbindungUndKI engine;
+
+    @Test
+    void berechneSchwierigeFormel_liefert_Antwort_auf_alles() {
+
+        // Given
+
+        Taschenrechner taschenrechner = new Taschenrechner(engine);
+
+        when(engine.addiere(anyInt())).thenReturn(42L); // Stubbing
+
+        // When
+
+        String ergebnis = taschenrechner.berechneSchwierigeFormel();
+
+        // Then
+
+        assertThat(ergebnis).isEqualTo("Antwort: 42");
+    }
+}
