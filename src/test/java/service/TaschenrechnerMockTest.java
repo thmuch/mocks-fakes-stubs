f8e9e1ea5b218a3d6240b474b3e3388c06b8c0ac
Thomas Much <github@snailshell.de>
2023-09-10 13:21:12 +0200

    Add mock test

---
+++ src/test/java/service/TaschenrechnerMockTest.java
@@ -0,0 +1,40 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TaschenrechnerMockTest {
+
+    @Mock
+    RechenengineMitDatenbankanbindungUndKI engine;
+
+    @Test
+    void berechneSchwierigeFormel_liefert_Antwort_auf_alles() {
+
+        // Given
+
+        Taschenrechner taschenrechner = new Taschenrechner(engine);
+
+        when(engine.addiere(anyInt())).thenReturn(42L); // Stubbing...
+
+        // When
+
+        String ergebnis = taschenrechner.berechneSchwierigeFormel();
+
+        // Then
+
+        verify(engine, times(1)).initialisiereMit(2); // Mock-Verifications
+        verify(engine).multipliziereMit(3);
+        verify(engine).addiere(36);
+        verifyNoMoreInteractions(engine);
+
+        assertThat(ergebnis).isEqualTo("Antwort: 42"); // doch Stubbing nötig, s.o.
+    }
+}
